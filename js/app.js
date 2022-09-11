12b550ae925ef2520e42cda5a833d2f2a5bfed51
ManasSarker <manas.sarker.moon@gmail.com>
2022-09-11 23:05:50 +0600

    solved the comments sections

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
   return text.length < 30
-    ? "text"
+    ? text
     : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -59,7 +59,7 @@ const createPost = (post) => {
 
   const userImage = post.userImage;
 
-  console.log("They are ", post);
+  console.log("They are ", post.comments[0].user);
 
   const div = document.createElement("article");
   div.classList.add("post");
@@ -133,9 +133,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
